c2f36a25b4b03bbeeb9e32e6c06ce6c54e6acf76
artem.chuzhmarov <artem.chuzhmarov@gmail.com>
2016-10-06 20:16:09 +0300

    *rewrite text category using

--- WTFChat/Service/Business/TextCategoryService.swift
+++ WTFChat/Service/Business/TextCategoryService.swift
@@ -1,5 +1,57 @@
 import Foundation
 
+enum TextDifficulty : Int {
+    case Easy = 0
+    case Normal
+    case Hard
+
+    var levelsCount : Int {
+        get {
+            switch self {
+            case .Easy:
+                return 6
+            case .Normal:
+                return 12
+            case .Hard:
+                return 6
+            }
+        }
+    }
+
+    var firstLevelId: Int {
+        get {
+            switch self {
+            case .Easy:
+                return 1
+            case .Normal:
+                return 1 + TextDifficulty.Easy.levelsCount
+            case .Hard:
+                return 1 + TextDifficulty.Easy.levelsCount + TextDifficulty.Normal.levelsCount
+            }
+        }
+    }
+
+    var levelsPerLength : Int {
+        get {
+            return self.levelsCount / 3
+        }
+    }
+
+    static func getAll() -> [TextDifficulty] {
+        return [.Easy, .Normal, .Hard]
+    }
+}
+
+enum TextLength : Int {
+    case Short = 0
+    case Medium
+    case Long
+
+    static func getAll() -> [TextLength] {
+        return [.Short, .Medium, .Long]
+    }
+}
+
 class TextCategory {
     var title: String = ""
     private var texts = [String]()
@@ -46,13 +98,14 @@ class TextCategoryService: Service {
     private let TITLE_INDEX = 1
     private let BASE_RU_TEXT_PATH = "/Texts/ru/"
 
-    private var textsMap = [CipherType: [Int: TextCategory]]()
-    //private var textCategories = [TextCategory]()
+    private var textsMap = [TextDifficulty: [TextLength: [TextCategory]]]()
+
+    private let textLengths = TextLength.getAll()
 
     override func initService() {
-        if let cipherFolders = getFilesList(BASE_RU_TEXT_PATH) {
-            for cipherFolder in cipherFolders {
-                loadCipherTexts(BASE_RU_TEXT_PATH, cipherFolder: cipherFolder)
+        if let difficultyFolders = getFilesList(BASE_RU_TEXT_PATH) {
+            for difficultyFolder in difficultyFolders {
+                loadDifficultyTexts(BASE_RU_TEXT_PATH, difficultyFolder: difficultyFolder)
             }
         }
     }
@@ -70,52 +123,42 @@ class TextCategoryService: Service {
         }
     }
 
-    private func loadCipherTexts(basePath: String, cipherFolder: String) {
-        let cipherType = getCipherTypeFromFolderName(cipherFolder)
-        if (cipherType == nil) {
+    private func loadDifficultyTexts(basePath: String, difficultyFolder: String) {
+        let difficulty = getDifficultyFromFolderName(difficultyFolder)
+        if (difficulty == nil) {
             return
         }
 
-        textsMap[cipherType!] = [Int: TextCategory]()
+        textsMap[difficulty!] = [TextLength: [TextCategory]]()
 
-        let difficultyFoldersPath = basePath + cipherFolder + "/"
-
-        if let difficultyFolders = getFilesList(difficultyFoldersPath) {
-            for difficultyFolder in difficultyFolders {
-                loadTextCategoriesFromFolder(difficultyFoldersPath, difficultyFolder: difficultyFolder, cipherType: cipherType!)
-            }
-        }
-    }
-
-    private func getCipherTypeFromFolderName(cipherFolder: String) -> CipherType? {
-        let cipherTypeString = cipherFolder.componentsSeparatedByString("_")[0]
-
-        let cipherTypeRaw = Int(cipherTypeString)
-        if (cipherTypeRaw == nil) {
-            print("Can't get cipherType from folder name: \(cipherFolder)")
-            return nil
-        }
-
-        let cipherType = CipherType(rawValue: cipherTypeRaw!)
-        if (cipherType == nil) {
-            print("Can't get cipherType from folder name: \(cipherFolder)")
-            return nil
-        }
-
-        return cipherType
-    }
-
-    private func loadTextCategoriesFromFolder(basePath: String, difficultyFolder: String, cipherType: CipherType) {
         let textCategoriesFilesPath = basePath + difficultyFolder + "/"
 
         if let textCategoriesFiles = getFilesList(textCategoriesFilesPath) {
             for textCategoryFile in textCategoriesFiles {
-                loadTextCategory(textCategoriesFilesPath, fileName: textCategoryFile, cipherType: cipherType)
+                loadTextCategory(textCategoriesFilesPath, fileName: textCategoryFile, textDifficulty: difficulty!)
             }
         }
     }
 
-    private func loadTextCategory(basePath: String, fileName: String, cipherType: CipherType) {
+    private func getDifficultyFromFolderName(difficultyFolder: String) -> TextDifficulty? {
+        let difficultyFolderString = difficultyFolder.componentsSeparatedByString("_")[0]
+
+        let difficultyRaw = Int(difficultyFolderString)
+        if (difficultyRaw == nil) {
+            print("Can't get difficulty from folder name: \(difficultyFolder)")
+            return nil
+        }
+
+        let difficulty = TextDifficulty(rawValue: difficultyRaw!)
+        if (difficulty == nil) {
+            print("Can't get difficulty from folder name: \(difficultyFolder)")
+            return nil
+        }
+
+        return difficulty
+    }
+
+    private func loadTextCategory(basePath: String, fileName: String, textDifficulty: TextDifficulty) {
         let url = getFileUrl(basePath, fileName: fileName)
 
         if let loadedData = Url.open(url) {
@@ -139,17 +182,16 @@ class TextCategoryService: Service {
 
             messages = messages.sort({$0.characters.count < $1.characters.count})
 
-            let lvlIds = getLvlIdsFromFileName(fileName)
-            let step = messages.count / lvlIds.count
+            let step = messages.count / textLengths.count
 
-            for i in 0 ..< lvlIds.count {
+            for i in 0 ..< textLengths.count {
                 let newCategory = TextCategory()
                 newCategory.title = title
 
                 let leftBorder = step * i
 
                 let rightBorder: Int
-                if (i == lvlIds.count - 1) {
+                if (i == textLengths.count - 1) {
                     rightBorder = messages.count
                 } else {
                     rightBorder = step * (i + 1)
@@ -159,31 +201,15 @@ class TextCategoryService: Service {
                     newCategory.appendText(messages[i])
                 }
 
-                textsMap[cipherType]![lvlIds[i]] = newCategory
+                if (textsMap[textDifficulty]![textLengths[i]] == nil) {
+                    textsMap[textDifficulty]![textLengths[i]] = [TextCategory]()
+                }
+
+                textsMap[textDifficulty]![textLengths[i]]!.append(newCategory)
             }
         } else {
             print("error reading file \(url.path)")
         }
-    }
-
-    private func getLvlIdsFromFileName(fileName: String) -> [Int] {
-        let levelsString = fileName.componentsSeparatedByString(".")[0]
-        let levelsBorders = levelsString.componentsSeparatedByString("-")
-        let lowLvlBorder = Int(levelsBorders[0])
-        let highLvlBorder = Int(levelsBorders[1])
-
-        if (lowLvlBorder == nil || highLvlBorder == nil) {
-            print("Can't get levels from file name: \(fileName)")
-            return [Int]()
-        }
-
-        var result = [Int]()
-
-        for i in lowLvlBorder!...highLvlBorder! {
-            result.append(i)
-        }
-
-        return result
     }
 
     private func getFileUrl(basePath: String, fileName: String) -> NSURL {
@@ -192,7 +218,34 @@ class TextCategoryService: Service {
     }
 
     func getTextCategoryForLevel(level: Level) -> TextCategory? {
-        let cipherType = level.category.cipherType
-        return textsMap[cipherType]![level.id]
+        let textDifficulty = getTextDifficultyForLevel(level)
+        let textLength = getTextLengthForLevel(level, textDifficulty: textDifficulty)
+
+        let texts = textsMap[textDifficulty]![textLength]!
+        let textIndex = getTextIndexForLevel(level, textDifficulty: textDifficulty, textCount: texts.count)
+
+        print(String(textDifficulty) + " " + String(textLength) + " " + String(textIndex))
+
+        return texts[textIndex]
+    }
+
+    private func getTextDifficultyForLevel(level: Level) -> TextDifficulty {
+        if (level.id < TextDifficulty.Normal.firstLevelId) {
+            return .Easy
+        } else if (level.id < TextDifficulty.Hard.firstLevelId) {
+            return .Normal
+        } else {
+            return .Hard
+        }
+    }
+
+    private func getTextLengthForLevel(level: Level, textDifficulty: TextDifficulty) -> TextLength {
+        let levelCategory: Int = (level.id - textDifficulty.firstLevelId) / textDifficulty.levelsPerLength
+        return TextLength(rawValue: levelCategory)!
+    }
+
+    private func getTextIndexForLevel(level: Level, textDifficulty: TextDifficulty, textCount: Int) -> Int {
+        let cipherTypeRaw = level.category.cipherType.rawValue
+        return (level.id - textDifficulty.firstLevelId + cipherTypeRaw * textDifficulty.levelsCount) % textCount
     }
 }
